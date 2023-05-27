cf0fb9947ecc02ecd168241c59465e7538b05095
Guilherme Gomes <guilherme.gsr.02@gmail.com>
2023-05-27 17:16:22 -0300

    Adição da função que compara inteiros

--- models/pokemon.h
+++ models/pokemon.h
@@ -28,9 +28,22 @@ void print_pokemons(pokemon p)
     cout << "*-----------------------------------------------------*" << endl;
 }
 
+//Função responsável por comparar 2 strings
 int compare_pokemons(pokemon p1, pokemon p2)
 {
     return p1.name.compare(p2.name);
+}
+
+//Função responsável por comparar dois ids
+template <typename T>
+int compare_by_id(T element, int target)
+{
+    if (element.id < target)
+        return -1;
+    else if (element.id > target)
+        return 1;
+    else
+        return 0;
 }
 
 //Função responsável por carregar os itens do arquivo.csv para a lista
@@ -60,6 +73,7 @@ void read_database(list<T>& lst)
     data.close();
 }
 
+//Função responsável por salvar no arquivo .csv
 template <typename T>
 void save_on_database(list<T>& lst)
 {
