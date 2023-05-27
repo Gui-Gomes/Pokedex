308987686f236f231cc21bc95e8ed1b40cd84894
Guilherme Gomes <guilherme.gsr.02@gmail.com>
2023-05-27 12:51:01 -0300

    Adição da função quicksort

--- app/app.h
+++ app/app.h
@@ -63,17 +63,20 @@ node<T>* get_node(list<T>& lst, int position)
 
 //Função responsável por remover um item da lista.
 //Apresenta um bug que remove os itens anteriores.
-template<typename T>
+template <typename T>
 void remove_node(list<T>& lst, int position)
 {
     if (position < 0 || position >= lst.cont)
     {
-        cout << "Invalid position!" << endl;
-        return;
+        cout << "Invalid position!" << endl << endl;
     }
     node<T>* aux = get_node(lst, position);
     if (aux->next != NULL)
-        aux->next->previous = aux->next;
+        aux->next->previous = aux->previous;
+    else
+        lst.end = aux->previous;
+    if (aux->previous != NULL)
+        aux->previous->next = aux->next;
     else
         lst.begin = aux->next;
     delete aux;
@@ -94,7 +97,7 @@ void print(list<T>& lst, void(*print_function)(T))
 
 //Função responsável por trocar itens na lista.
 template<typename T>
-void switch_nodes(list<T>& lst, int position1, int position2)
+void swap_nodes(list<T>& lst, int position1, int position2)
 {
     node<T>* node1 = get_node(lst, position1);
     node<T>* node2 = get_node(lst, position2);
@@ -117,8 +120,38 @@ void bubble_sort(list<T>& lst, int(*sort_function)(T,T))
             if (sort_function(node1->data, node2->data) > 0)
             {
                 sort = false;
-                switch_nodes(lst, i, i - 1);
+                swap_nodes(lst, i, i - 1);
             }
         }
+    }
+}
+
+//Função quicksort responsável por organizar a lista em ordem de ID;
+template <typename T>
+void quicksort(list<T>& lst, int left, int right)
+{
+    if (left < right)
+    {
+        int i = left;
+        int j = right;
+        int pivotIndex = left + (right - left) / 2;
+        node<T>* pivotNode = get_node(lst, pivotIndex);
+        while (i <= j)
+        {
+            while (get_node(lst, i)->data.id < pivotNode->data.id)
+                i++;
+            while (get_node(lst, j)->data.id > pivotNode->data.id)
+                j--;
+            if (i <= j)
+            {
+                swap_nodes(lst, i, j);
+                i++;
+                j--;
+            }
+        }
+        if (left < j)
+            quicksort(lst, left, j);
+        if (i < right)
+            quicksort(lst, i, right);
     }
 }
